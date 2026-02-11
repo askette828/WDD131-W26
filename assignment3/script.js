c70604d89c56b4c2c875f2e0555f1c6b6526292f
askette828 <seitzhandias@gmail.com>
2026-02-11 09:45:44 -0700

    resubmitting W02 Ponder: Computational Thinking
    resubmitting W02 Ponder: Computational Thinking

--- assignment3/script.js
+++ assignment3/script.js
@@ -9,13 +9,13 @@ function changeTheme() {
     let current = selectElem.value;
     
     if (current === 'ocean') {
-        document.body.style.backgroundImage = "url(images/image.png)";
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/ocean.jpg')";
         pageContent.style.fontFamily = "Papyrus, fantasy";
     } else if (current === 'forest') {
-        document.body.style.backgroundImage = "url(images/image2.png)";
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/forest.jpg')";
         pageContent.style.fontFamily = "Impact, sans-serif";
     } else if (current === 'desert') {
-        document.body.style.backgroundImage = "url(images/image3.png)";
+        document.body.style.backgroundImage = "url('https://wddbyui.github.io/wdd131/images/desert.jpg')";
         pageContent.style.fontFamily = "'Big Caslon', serif";
     } else {
         document.body.style.backgroundImage = "none";
